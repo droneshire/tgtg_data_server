46ca64aa97ef99bc2115d6d8eb9d37ef077de340
Ross Yeager <ryeager12@gmail.com>
2023-08-27 15:46:35 -0700

    fix admin and remove prints

--- src/components/dashboard/views/preferencesTabs/NotificationsTab.tsx
+++ src/components/dashboard/views/preferencesTabs/NotificationsTab.tsx
@@ -15,7 +15,6 @@ import {
   FirestoreBackedTimeZoneSelect,
 } from "components/utils/forms";
 import { isValidEmail } from "utils/validators";
-import { ADMIN_USERS } from "utils/constants";
 const NotificationsTab: FC<{
   userConfigSnapshot: DocumentSnapshot<ClientConfig>;
 }> = ({ userConfigSnapshot }) => {
@@ -24,9 +23,6 @@ const NotificationsTab: FC<{
     <>
       <Typography variant="h6" gutterBottom>
         Email
-      </Typography>
-      <Typography variant="body2" gutterBottom>
-        {"Admins: " + ADMIN_USERS.join(", ")}
       </Typography>
       <FormGroup>
         <FirestoreBackedTextField
