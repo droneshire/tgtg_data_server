200c2c2ecbc2b1e90d5bd776a9ae808261e71325
Ross Yeager <ryeager12@gmail.com>
2023-08-30 22:11:31 -0700

    updated packages and constants to support google api

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 // admin user ids for feature flagging
 export const ADMIN_USERS =  process.env.REACT_APP_ADMIN_USERS?.split(",") ?? [];
+export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? "";
