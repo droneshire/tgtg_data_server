a9d0e823fe7fe7fc80700cc95d4a1d9bddaa8f3f
Ross Yeager <ryeager12@gmail.com>
2024-02-18 17:35:56 -0800

    rename and more info

--- src/components/dashboard/views/analysisTabs/components/ResearchCoverage.tsx
+++ src/components/dashboard/views/analysisTabs/components/ResearchCoverage.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { DataMaps } from "./CsvDataUploader";
-import { Box, Button, Slider, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Slider,
+  Typography,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
 import {
   CostResults,
   calculateCostFromResults,
@@ -17,7 +25,7 @@ const ProximitySearchCoverage: React.FC<ProximitySearchCoverageProps> = ({
 }) => {
   const [searchGridWitdthMeters, setSearchGridWidthMeters] =
     React.useState(500.0);
-  const [costPerSearch, setCostPerSearch] = React.useState(0.04);
+  const [costPerSearch, setCostPerSearch] = React.useState(0.005);
   const [searchRadiusMiles, setSearchRadiusMiles] = React.useState(20.0);
 
   const onClick = () => {
@@ -37,6 +45,21 @@ const ProximitySearchCoverage: React.FC<ProximitySearchCoverageProps> = ({
     setSearchRadiusMiles(value as number);
   };
 
+  const handleCostPerSearchChange = (
+    event: SelectChangeEvent<string>,
+    child: React.ReactNode
+  ) => {
+    setCostPerSearch(parseFloat(event.target.value));
+  };
+
+  const step = 0.005;
+  const max = 0.1;
+
+  const costPerClickValues = [];
+  for (let value = 0.005; value <= max; value += step) {
+    costPerClickValues.push(value.toFixed(3));
+  }
+
   return (
     <>
       <Box
@@ -47,7 +70,11 @@ const ProximitySearchCoverage: React.FC<ProximitySearchCoverageProps> = ({
           alignItems: "flex-start",
         }}
       >
-        <Typography id="radius-slider" gutterBottom>
+        <Typography
+          id="radius-slider"
+          gutterBottom
+          sx={{ marginBottom: "16px" }}
+        >
           Search Radius (mi)
         </Typography>
         <Slider
@@ -58,10 +85,35 @@ const ProximitySearchCoverage: React.FC<ProximitySearchCoverageProps> = ({
           step={0.5}
           aria-labelledby="radius-slider"
           valueLabelDisplay="auto"
+          sx={{ marginBottom: "16px" }}
         />
-
-        <Button variant="contained" component="label" onClick={onClick}>
-          ProximitySearchCoverage
+        <Typography
+          id="cost-per-click-select"
+          gutterBottom
+          sx={{ marginBottom: "16px" }}
+        >
+          Price per Search
+        </Typography>
+        <Select
+          value={costPerSearch.toFixed(3)}
+          onChange={handleCostPerSearchChange}
+          labelId="cost-per-click-select"
+          id="cost-per-click-select"
+          sx={{ marginBottom: "16px" }}
+        >
+          {costPerClickValues.map((value) => (
+            <MenuItem key={value} value={value}>
+              ${value}
+            </MenuItem>
+          ))}
+        </Select>
+        <Button
+          variant="contained"
+          component="label"
+          onClick={onClick}
+          sx={{ marginBottom: "16px" }}
+        >
+          Analyze
         </Button>
       </Box>
     </>
